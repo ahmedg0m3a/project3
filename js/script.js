6abff16ab3fbf9f355e5148bd43e55b7941ab4ea
AhmedRedaGomaa <//github.com/ahmedg0m3a/ahmedg0m3a.git>
2024-11-03 18:34:04 +0200

    create project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.getElementById("btn").onclick = function(){
+    alert("تم النسخ")
+}
+document.getElementById("btn2").onclick = function(){
+    alert("تم النسخ")
+}     
+document.getElementById("btn3").onclick = function(){
+    alert("تم النسخ")
+}
+//  <div class="container col mt-5 m-auto position-relative" >
+// <div class="paernt col position-relative">
+//   <div class="img position-absolute end-0  row-12 row-sm-6 row-md-4 row-lg-4 " style="max-width:35%;" >
+//     <img src="img/Miramar residence El gouna 750 8_495ea_lg.jpg" alt="" width="350" height="400" class="rounded-end-3 cut">
+//   </div>
+//   <div class="p-5 inf  row-12 row-sm-6 row-md-4 row-lg-4 ">
+//     <h2 class="text-info-emphasis">Podcast Titel</h2>
+//     <i class="fas fa-calendar-alt">mon, May 25th 2024</i>
+//     <div class="line bg-primary rounded-3 mt-3"></div>
+//     <p class="fs-5 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum quibusdam ut exercitationem aliquid aspernatur totam, vitae, minus beatae facilis repellat voluptate, necessitatibus placeat excepturi perferendis quo deleniti quisquam neque temporibus.</p>
+//     <button class="btn bg-secondary-subtle mt-5"><i class="fas fa-tag"></i>Podcast</button>
+//     <button class="btn bg-secondary-subtle mt-5"><i class="far fa-clock"></i>55 mins</button>
+//     <button class="btn bg-secondary-subtle mt-5"><i class="fas fa-play"></i></i>Play Episode</button>
+//   </div>
+// </div>
+// </div>
